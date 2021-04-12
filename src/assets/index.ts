ba42dbe10e6f8f2e2181d53dee92539f6df40b25
KIM Kangseong <KIM.Kangseong@gmail.com>
2021-04-12 21:57:21 +0900

    REFACTORING 이미지 경로 및 정보 수정

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,17 +1,18 @@
 import { championByKey } from 'consts';
 
-const BASE = 'public/images'
+const BASE = 'public'
 
 export const getImagePath = (category: string, id: string) => {
   switch(category) {
     case 'profileicon':
-      return `${BASE}/${category}/${id}.png`;
+      return `${BASE}/dragontail/latest/img/profileicon/${id}.png`;
     case 'emblem':
-      return `${BASE}/emblems/Emblem_${id.replace(/$[a-z]/, (match, p1) => p1.toUpperCase())}.png`;
+      return `${BASE}/images/emblems/Emblem_${id.replace(/$[a-z]/, (match, p1) => p1.toUpperCase())}.png`;
     case 'champion':
-      return `${BASE}/${category}/${championByKey.data[id as keyof typeof championByKey.data].id}.png`;
+      // return `${BASE}/images/${category}/${championByKey[id as keyof typeof championByKey].id}.png`;
+      return `${BASE}/dragontail/latest/img/champion/${championByKey[id as keyof typeof championByKey].id}.png`;
     default:
-      return `${BASE}/${category}/${id}.png`;
+      return `${BASE}/images/${category}/${id}.png`;
   }
   return ''
 };
